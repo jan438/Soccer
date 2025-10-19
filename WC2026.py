fbb11450f76ec8ac9d625ada57f857d3e72747be
jan438 <jan438@users.noreply.github.com>
2025-10-19 16:00:26 +0200

    code simplified

--- WC2026.py
+++ WC2026.py
@@ -58,7 +58,7 @@ width = 595
 height = 842
 poule_width = 48
 poule_height = 200
-pouleland_height = 50
+pouleland_height = 40
 outsidearea = "#9e9e9e"
 left_margin = 9.4
 poulerect_y = 590
@@ -98,7 +98,7 @@ for poule in range(12):
     my_canvas.drawString(left_margin + poule * poule_width, poulerect_y + poule_height - 17, chr(65 + poule))
     my_canvas.rect(left_margin + poule * poule_width, poulerect_y, poule_width, poule_height, stroke = 1, fill = 0)
     for team in range(teamspp):
-        my_canvas.rect(left_margin + poule * poule_width + poule_margin, pouleland_y + poule_margin, poule_width - 2 * poule_margin, pouleland_height - 2 * poule_margin, stroke = 1, fill = 0)
+        my_canvas.rect(left_margin + poule * poule_width + poule_margin, pouleland_y + poule_margin, poule_width - 2 * poule_margin, pouleland_height, stroke = 1, fill = 0)
         drawing = scaleSVG("Logos/" + nationsdata[teamcounter][0] + ".svg", float(nationsdata[teamcounter][1]))
         renderPDF.draw(drawing, my_canvas, poule_x + float(nationsdata[teamcounter][2]), pouleland_y +  float(nationsdata[teamcounter][3]))
         my_canvas.setFont(socfont, 12)
