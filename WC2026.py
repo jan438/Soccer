c17e0c35f9232db53ce191ebd992c28499d9bf23
jan438 <jan438@users.noreply.github.com>
2025-10-20 09:52:46 +0200

    print removed

--- WC2026.py
+++ WC2026.py
@@ -98,7 +98,6 @@ for poule in range(12):
     my_canvas.rect(left_margin + poule * poule_width, poulerect_y, poule_width, poule_height, stroke = 1, fill = 0)
     for team in range(teamspp):
         nameinlogo = nationsdata[teamcounter][4]
-        print(len( nameinlogo  ), nameinlogo)
         my_canvas.rect(left_margin + poule * poule_width + poule_margin, pouleland_y, poule_width - 2 * poule_margin, pouleland_height, stroke = 1, fill = 0)
         drawing = scaleSVG("Logos/" + nationsdata[teamcounter][0] + ".svg", float(nationsdata[teamcounter][1]))
         renderPDF.draw(drawing, my_canvas, poule_x + float(nationsdata[teamcounter][2]), pouleland_y +  float(nationsdata[teamcounter][3]))
