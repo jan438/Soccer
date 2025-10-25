c6d9f78b454c3cbddc7e65439bebf2ed58705f20
jan438 <jan438@users.noreply.github.com>
2025-10-25 11:12:11 +0200

    wiki namr changen

--- WC2026.py
+++ WC2026.py
@@ -68,6 +68,7 @@ poule_margin = 5
 cadre_pouleland = False
 maxnamewidth = 42.0
 scalewiki = 0.5
+scalesimple = 0.5
 
 countnations = 48
 
@@ -82,7 +83,7 @@ my_canvas.setTitle("World Cup 2026")
 my_canvas.setFillColor(HexColor(outsidearea))
 my_canvas.rect(left_padding, bottom_padding, width, height, fill = 1)
 
-drawing = scaleSVG('SVG/WorldMap.svg', scalewiki)
+drawing = scaleSVG('SVG/WorldMapWiki.svg', scalewiki)
 renderPDF.draw(drawing, my_canvas, 0, 100)
 drawing = scaleSVG('FIFA.svg', 0.1)
 renderPDF.draw(drawing, my_canvas, 50, 800)
