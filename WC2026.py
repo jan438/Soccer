c9115cbc44e19397371bff7ec12abd7d5d2c2d2b
jan438 <jan438@users.noreply.github.com>
2025-10-27 15:04:48 +0100

    black text added

--- WC2026.py
+++ WC2026.py
@@ -109,9 +109,10 @@ my_canvas.drawString(left_margin + 9.141, 195.789, "American Samoa")
 my_canvas.setFillColor(HexColor("#e88cff"))
 my_canvas.circle(left_margin + 9.141, 195.789, 4.0, stroke = 0, fill = 1)
 # New Zealand
+my_canvas.setFillColor(HexColor("#000000"))
 my_canvas.drawString(left_margin + 332.016 , 241.89, "New Zealand")
 my_canvas.setFillColor(HexColor("#e88cff"))
-my_canvas.circle(left_margin + 332.016 , 241.89, 4.0, stroke = 0, fill = 1)
+my_canvas.circle(left_margin + 432.016 , 241.89, 4.0, stroke = 0, fill = 1)
 
 for poule in range(12):
     my_canvas.setFillColor(HexColor("#b1b1b1"))
