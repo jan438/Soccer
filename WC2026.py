c62b72f93211d8807248eea7b93627b6020836b5
jan438 <jan438@users.noreply.github.com>
2025-10-30 11:10:54 +0100

    line corrected

--- WC2026.py
+++ WC2026.py
@@ -207,6 +207,8 @@ for j in range(13):
         drawing = scaleSVG("Clocks/2030tw.svg", 0.4)
         renderPDF.draw(drawing, my_canvas, left_margin + i * colwidthgame + 15, line)
     line -= 18
+    if j == 8:
+       my_canvas.line(5, line, 590, line)
     
 my_canvas.save()
 key = input("Wait")
