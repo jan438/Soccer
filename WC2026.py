8f8736662b19f480174a036933fdb652e36289dd
jan438 <jan438@users.noreply.github.com>
2025-10-31 14:23:52 +0100

    citynames drawed

--- WC2026.py
+++ WC2026.py
@@ -201,6 +201,8 @@ for i in range(len(gameevents)):
 for i in range(len(cities)):    
     my_canvas.setFillColor(HexColor(cities[i][1]))
     my_canvas.circle(float(cities[i][2][0]), float(cities[i][2][1]), 2.0, stroke = 0, fill = 1)
+    my_canvas.setFillColor(HexColor("#000000"))  
+    my_canvas.drawString(float(cities[i][2][0]) + 5.0, float(cities[i][2][1]), cities[i][0])
 
 line = 550
 for j in range(13):   
