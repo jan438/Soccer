8ceba0417b76cd9b47ca1325a24ee39b53bb2832
jan438 <jan438@users.noreply.github.com>
2025-11-03 09:43:07 +0100

    citycircke more to right

--- WC2026.py
+++ WC2026.py
@@ -251,7 +251,7 @@ for j in range(12):
         if calindex < limitcalindex:
             locidx = lookuplocation(gameevents[calindex].location)
             my_canvas.setFillColor(HexColor(cities[locidx][1]))
-            my_canvas.circle(left_margin + i * colwidthgame + 8.2, line + 12.0, 2.0, stroke = 0, fill = 1)
+            my_canvas.circle(left_margin + i * colwidthgame + 9.0, line + 12.0, 2.0, stroke = 0, fill = 1)
         if calindex < limitcalindex:
             calindex += 1
     line -= 18
