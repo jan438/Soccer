7305ebd9b176649390102080d47612c03eb9ecfb
jan438 <jan438@users.noreply.github.com>
2025-11-05 09:33:31 +0100

    desciption added

--- WC2026.py
+++ WC2026.py
@@ -247,6 +247,8 @@ for j in range(13):
         my_canvas.setFillColor(HexColor("#ffffff"))
         if category == "3" or category == "1" or category == "Q" or category == "S" or category == "T" or category == "Z":
             my_canvas.drawString(left_margin + i * colwidthgame + 1.0, line + 9.5, gameevents[calindex].summary[1:4])
+            description = gameevents[calindex].description
+            print("Description", description)
         else:
             my_canvas.drawString(left_margin + i * colwidthgame + 1.0, line + 9.5, category)
         my_canvas.setFillColor(HexColor("#000000"))
