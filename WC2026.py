28dec0437a291c8e3603e50556c81d1bd1c8fce1
jan438 <jan438@users.noreply.github.com>
2025-11-05 11:14:43 +0100

    group stage added

--- WC2026.py
+++ WC2026.py
@@ -234,10 +234,10 @@ limitcalindex = 103
 gameindex = 0
 
 line = 550
-my_canvas.drawString(left_margin + 1.0, line + 10, "Group Stage")
-my_canvas.drawString(left_margin + 1.0, line + 20, "Round of 32")
-my_canvas.drawString(left_margin + 1.0, line + 30, "Round of 16")
-my_canvas.drawString(left_margin + 1.0, line + 40, "Quarter finals")
+my_canvas.drawString(left_margin + 1.0, line + 20, "Group Stage")
+my_canvas.drawString(left_margin + 1.0, line - 20, "Round of 32")
+my_canvas.drawString(left_margin + 1.0, line - 30, "Round of 16")
+my_canvas.drawString(left_margin + 1.0, line - 40, "Quarter finals")
 #my_canvas.drawString(left_margin + i * colwidthgame + 1.0, line + 9.5, "Semi finals")
 #my_canvas.drawString(left_margin + i * colwidthgame + 1.0, line + 9.5, "Bronze final")
 #my_canvas.drawString(left_margin + i * colwidthgame + 1.0, line + 9.5, "Final")
