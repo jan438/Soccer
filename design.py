8e098525af94583f922f5d87dd73843fecfa1387
jan438 <jan438@users.noreply.github.com>
2025-11-06 09:37:24 +0100

    spacefor 30 designgs

--- design.py
+++ design.py
@@ -21,14 +21,14 @@ from reportlab.graphics.shapes import *
 from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
 socfont = "LiberationSerif"
-Borealis = [ "#01EFAC", "#01CBAE", "#2082A6", "#524094", "#562A83" ]
-BluePlanet1 = [ "#2297FA", "#50B6FE", "#7ED8FA", "#94AEFE", "#8082D6" ]
-Oasis = [ "#23570D", "#367C28", "#86B06B", "#BAD5EC", "#6BA2DE" ]
+Borealis = [ "Borealis", "#01EFAC", "#01CBAE", "#2082A6", "#524094", "#562A83" ]
+BluePlanet1 = [ "BluePlanet1", "#2297FA", "#50B6FE", "#7ED8FA", "#94AEFE", "#8082D6" ]
+Oasis = [ "Oasis", "#23570D", "#367C28", "#86B06B", "#BAD5EC", "#6BA2DE" ]
 
 design_x = 10
-design_y = 800
-width = 50
-height = 50
+design_y = 700
+width = 30
+height = 30
 
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Soccer'
@@ -44,37 +44,37 @@ my_canvas = canvas.Canvas("PDF/Designs.pdf")
 
 my_canvas.setTitle("Designs")
 
-my_canvas.setFillColor(HexColor(Borealis[0]))
-my_canvas.rect(design_x, design_y, width, height, fill = 1)
 my_canvas.setFillColor(HexColor(Borealis[1]))
-my_canvas.rect(design_x + width, design_y, width, height, fill = 1)
+my_canvas.rect(design_x, design_y, width, height, fill = 1)
 my_canvas.setFillColor(HexColor(Borealis[2]))
-my_canvas.rect(design_x + 2 * width, design_y, width, height, fill = 1)
+my_canvas.rect(design_x + width, design_y, width, height, fill = 1)
 my_canvas.setFillColor(HexColor(Borealis[3]))
-my_canvas.rect(design_x + 3 * width, design_y, width, height, fill = 1)
+my_canvas.rect(design_x + 2 * width, design_y, width, height, fill = 1)
 my_canvas.setFillColor(HexColor(Borealis[4]))
+my_canvas.rect(design_x + 3 * width, design_y, width, height, fill = 1)
+my_canvas.setFillColor(HexColor(Borealis[5]))
 my_canvas.rect(design_x + 4 * width, design_y, width, height, fill = 1)
-design_y = design_y - 50
-my_canvas.setFillColor(HexColor(BluePlanet1[0]))
-my_canvas.rect(design_x, design_y, width, height, fill = 1)
+design_y = design_y - 60
 my_canvas.setFillColor(HexColor(BluePlanet1[1]))
-my_canvas.rect(design_x + width, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(BluePlanet1[2]))
-my_canvas.rect(design_x + 2 * width, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(BluePlanet1[3]))
-my_canvas.rect(design_x + 3 * width, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(BluePlanet1[4]))
-my_canvas.rect(design_x + 4 * width, design_y, width, height, fill = 1)
-design_y = design_y - 50
-my_canvas.setFillColor(HexColor(Oasis[0]))
 my_canvas.rect(design_x, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(Oasis[1]))
+my_canvas.setFillColor(HexColor(BluePlanet1[2]))
 my_canvas.rect(design_x + width, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(Oasis[2]))
+my_canvas.setFillColor(HexColor(BluePlanet1[3]))
 my_canvas.rect(design_x + 2 * width, design_y, width, height, fill = 1)
-my_canvas.setFillColor(HexColor(Oasis[3]))
+my_canvas.setFillColor(HexColor(BluePlanet1[4]))
 my_canvas.rect(design_x + 3 * width, design_y, width, height, fill = 1)
+my_canvas.setFillColor(HexColor(BluePlanet1[5]))
+my_canvas.rect(design_x + 4 * width, design_y, width, height, fill = 1)
+design_y = design_y - 60
+my_canvas.setFillColor(HexColor(Oasis[1]))
+my_canvas.rect(design_x, design_y, width, height, fill = 1)
+my_canvas.setFillColor(HexColor(Oasis[2]))
+my_canvas.rect(design_x + width, design_y, width, height, fill = 1)
+my_canvas.setFillColor(HexColor(Oasis[3]))
+my_canvas.rect(design_x + 2 * width, design_y, width, height, fill = 1)
 my_canvas.setFillColor(HexColor(Oasis[4]))
+my_canvas.rect(design_x + 3 * width, design_y, width, height, fill = 1)
+my_canvas.setFillColor(HexColor(Oasis[5]))
 my_canvas.rect(design_x + 4 * width, design_y, width, height, fill = 1)
 
 my_canvas.save()
