48d5fabbada22018691dba58b59a8e3d2ab81e30
jan438 <jan438@users.noreply.github.com>
2025-11-08 11:26:59 +0100

    poule iets hoger

--- WC2026.py
+++ WC2026.py
@@ -271,7 +271,7 @@ for j in range(13):
             opponent2 = description[idx + 2:]
         else:
             my_canvas.setFillColor(HexColor("#ffffff"))
-            my_canvas.drawString(left_margin + i * colwidthgame + 5.0, line + 9.5, category)
+            my_canvas.drawString(left_margin + i * colwidthgame + 5.8, line + 10.0, category)
         my_canvas.setFillColor(HexColor("#000000"))
         daystr = str(gameevents[calindex].day)
         monthstr = str(gameevents[calindex].month)
