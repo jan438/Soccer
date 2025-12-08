ff2411bc5a7959fd6f113923616053c5a9d8ba93
jan438 <jan438@users.noreply.github.com>
2025-12-08 09:25:25 +0100

    print added

--- WC2026.py
+++ WC2026.py
@@ -295,6 +295,7 @@ for j in range(13):
             my_canvas.drawString(left_margin + i * colwidthgame + 0.45, line + 0.5, datestr)
         my_canvas.drawString(left_margin + i * colwidthgame + 30, line, opponent2)
         my_canvas.drawString(left_margin + i * colwidthgame + 30, line + 6, opponent1)
+        print(gameevents[calindex].starttime)
         drawing = scaleSVG("Clocks/2030tw.svg", 0.4)
         renderPDF.draw(drawing, my_canvas, left_margin + i * colwidthgame + 15, line)
         locidx = lookuplocation(gameevents[calindex].location)
