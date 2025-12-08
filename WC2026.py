daf7054ed34dd0f12f1dba533bf98bdc6080f531
jan438 <jan438@users.noreply.github.com>
2025-12-08 10:13:43 +0100

    print removed

--- WC2026.py
+++ WC2026.py
@@ -309,7 +309,6 @@ for j in range(13):
         strhour = "{:02d}".format(hour)
         strminute = "{:02d}".format(minute)
         startevent = strhour + strminute
-        print(gameevents[calindex].starttime, "hour", hour, "minute", minute, startevent)
         drawing = scaleSVG("Clocks/" + startevent + "tw.svg", 0.4)
         renderPDF.draw(drawing, my_canvas, left_margin + i * colwidthgame + 15, line)
         locidx = lookuplocation(gameevents[calindex].location)
