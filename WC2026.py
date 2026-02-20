d6ee2c05b26a8b4c9ccdcb6782c4777be1c4e8bc
jan438 <jan438@users.noreply.github.com>
2026-02-20 09:41:32 +0100

    nog een krer

--- WC2026.py
+++ WC2026.py
@@ -260,7 +260,7 @@ for j in range(13):
 drawHorizontalRect(my_canvas, 10, 5, legendawidth, legendarowheight * 12, "l", lcol1, lcol2)
 drawHorizontalRect(my_canvas, 220, 5, legendawidth, legendarowheight * 12, "l", lcol1, lcol2)    
 drawHorizontalRect(my_canvas, 375, 5, legendawidth, legendarowheight * 12, "l", lcol1, lcol2)
-drawHorizontalRect(my_canvas, 160, 90, legendawidth, legendarowheight * 4, "l", lcol1, lcol2)
+drawHorizontalRect(my_canvas, 150, 120, legendawidth, legendarowheight * 4, "l", lcol1, lcol2)
 drawHorizontalRect(my_canvas, 510, 120, legendawidth, legendarowheight * 8, "l", lcol1, lcol2)
     
 teamcounter = 0
