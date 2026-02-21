b633873c3d251831b50f1555ab47d58433cfac3b
jan438 <jan438@users.noreply.github.com>
2026-02-21 13:34:55 +0100

    all kols added

--- WC2026.py
+++ WC2026.py
@@ -431,7 +431,7 @@ for i in range(48):
         ly = legenda4y + legenda1l * legendarowheight + legenda2l * legendarowheight + legenda3l * legendarowheight + legenda4l * legendarowheight
     if i == legenda1l + legenda2l + legenda3l + legenda4l:
         lx = legenda5x
-        ly = legenda5y - legenda1l * legendarowheight - legenda2l * legendarowheight - legenda3l * legendarowheight - legenda4l * legendarowheight
+        ly = legenda5y + legenda1l * legendarowheight + legenda2l * legendarowheight + legenda3l * legendarowheight + legenda4l * legendarowheight + legenda5l * legendarowheight
     drawing = scaleSVG("Logos/" + nationsdata[teamcounter][0] + ".svg", float(nationsdata[teamcounter][1]) / 4)
     renderPDF.draw(drawing, my_canvas, lx + 10, ly - i * legendarowheight)
     drawing = scaleSVG("Flags/" + nationsdata[teamcounter][8] + "tw.svg", 0.25)
