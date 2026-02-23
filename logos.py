9d0606aca92ba064089697513ee2c50b3f0ed762
jan438 <jan438@users.noreply.github.com>
2026-02-23 10:21:47 +0100

    logos python file added

---
+++ logos.py
@@ -0,0 +1,27 @@
+import os
+import calendar
+from datetime import datetime, date, timedelta
+import pytz
+import os
+import sys
+import csv
+import math
+import unicodedata
+from ics import Calendar, Event
+from reportlab.graphics import renderPDF
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.colors import HexColor
+from reportlab.pdfbase import pdfmetrics  
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from reportlab.lib.colors import blue, green, black, red, pink, gray, brown, purple, orange, yellow, white, lightgrey
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.units import inch, mm
+from reportlab.graphics.shapes import *
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
+drawing = svg2rlg("Germany.svg")
+renderPDF.drawToFile(drawing, "Germany.pdf")
+renderPM.drawToFile(drawing, "Germany.png", fmt="PNG")
+key = input("Wait")
