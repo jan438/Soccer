fafcbec532a87483c95f8ced4305ebaa95afb09e
jan438 <jan438@users.noreply.github.com>
2026-02-25 14:29:14 +0100

    data extend

--- WC2026cadre.py
+++ WC2026cadre.py
@@ -99,6 +99,7 @@ with open(file_to_open, 'r') as file:
     count = 0
     for row in csvreader:
         nationsdata.append(row)
+        print(row[12])
         count += 1
 print("Count csv", count)
 
