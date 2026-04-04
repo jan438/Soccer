ba5e18a2a36fe2c2ff0c987c7539a040f52b905a
jan438 <jan438@users.noreply.github.com>
2026-04-04 09:10:01 +0200

    48 color added

--- distinctcolors.py
+++ distinctcolors.py
@@ -9,19 +9,28 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.colors import HexColor
 from reportlab.lib.units import inch, mm
 
+colors = ["#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
+          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
+          "#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
+          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
+          "#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
+          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
+          ]
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Soccer'
 if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Soccer"
 os.chdir(path)
 my_canvas = canvas.Canvas("PDF/distinctcolors.pdf")
-my_canvas.setFillColor(HexColor('#FECDE5'))
 left_padding = 100
 bottom_padding = 300
 width = 40
 height = 40
+i = 0
 for col in range(12):
    for row in range(4):
+       my_canvas.setFillColor(HexColor(colors[i]))
        my_canvas.rect(left_padding + col * width, bottom_padding + row * height, width, height, fill = 1)
+       i += 1
 my_canvas.save()
 key = input("Wait")
