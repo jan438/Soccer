11e1dd1d28f1c4bce037143db10568dc7a5c7d35
jan438 <jan438@users.noreply.github.com>
2026-04-04 09:54:01 +0200

    reorginazed

--- distinctcolors.py
+++ distinctcolors.py
@@ -11,12 +11,11 @@ from reportlab.lib.units import inch, mm
 import matplotlib.pyplot as plt
 import numpy as np
 
-colors = ["#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
-          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
-          "#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
-          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
-          "#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
-          "#A8A202","#158FAD","#14AAF5","#CD0027","#4073FF","#D38895","#884DFF","#AF38EB",
+colors = [
+"#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438","#A8A202","#158FAD","#14AAF5","#CD0027",
+"#4073FF","#D38895","#884DFF","#AF38EB","#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
+"#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438","#A8A202","#158FAD","#14AAF5","#CD0027",
+"#4073FF","#D38895","#884DFF","#AF38EB","#88255F","#DB4035","#FF9933","#FAD000","#AFB83B","#7ECC49","#E7E84F","#299438",
           ]
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Soccer'
@@ -29,8 +28,8 @@ bottom_padding = 300
 width = 40
 height = 40
 i = 0
-for col in range(12):
-   for row in range(4):
+for row in range(4):
+   for col in range(12):
        my_canvas.setFillColor(HexColor(colors[i]))
        my_canvas.rect(left_padding + col * width, bottom_padding + row * height, width, height, fill = 1)
        i += 1
