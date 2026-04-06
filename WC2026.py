4f7440896242415e39a574652a736755b78bfa4c
jan438 <jan438@users.noreply.github.com>
2026-04-06 14:46:03 +0200

    empty regel removed

--- WC2026.py
+++ WC2026.py
@@ -448,6 +448,5 @@ for i in range(48):
     my_canvas.circle(lx + 32, ly + 4.0 - i * legendarowheight, 2.0, stroke = 0, fill = 1)
     my_canvas.circle(float(nationsdata[teamcounter][6]), float(nationsdata[teamcounter][7]), 2.0, stroke = 0, fill = 1)
     teamcounter += 1
-
 my_canvas.save()
 key = input("Wait")
