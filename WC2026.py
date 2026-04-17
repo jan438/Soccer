560d8ad50adf9aaf899969903827e9b163c607a6
jan438 <jan438@users.noreply.github.com>
2026-04-17 13:12:59 +0200

    gray added

--- WC2026.py
+++ WC2026.py
@@ -218,7 +218,7 @@ pcol1 = HexColor("#b4e9ff") # poule light blue
 pcol2 = HexColor("#b0c2ff") # poule dark blue
 ccol1 = HexColor("#9e9e9e") # dark gray
 ccol2 = HexColor("#b0b0b0") # light gray
-lcol = HexColor("#b0ffaf") # legenda green
+lcol = HexColor("#d0d0d0") # legenda gray
 
 rowheightgame = 18
 gameline = 550
