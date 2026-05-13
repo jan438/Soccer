acbf6b11b3fdea730d20445cdb1360f16d10d1a2
jan438 <jan438@users.noreply.github.com>
2026-05-13 13:20:43 +0200

    processics added

---
+++ processics.py
@@ -0,0 +1,90 @@
+import os
+import calendar
+from datetime import datetime, date, timedelta
+import pytz
+import os
+import sys
+import csv
+import math
+import unicodedata
+from ics import Calendar, Event
+from reportlab.graphics import renderPDF
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.colors import HexColor
+from reportlab.pdfbase import pdfmetrics  
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from reportlab.lib.colors import blue, green, black, red, pink, gray, brown, purple, orange, yellow, white, lightgrey
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.units import inch, mm
+from reportlab.graphics.shapes import *
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+
+alleventslines = []
+gameevents = []
+
+class GameEvent:
+    def __init__(self, summary, day, description, location, starttime, endtime, month):
+        self.summary = summary
+        self.day = day
+        self.description = description
+        self.location = location
+        self.starttime = starttime
+        self.endtime = endtime
+        self.month = month
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Soccer'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Soccer"
+os.chdir(path)
+eventcal = "Calendar/WK2026.ics"
+in_file = open(os.path.join(path, eventcal), 'r')
+count = 0
+lastpos = 0
+found = 0
+for line in in_file:
+    newlinepos = line.find("\t\n")
+    lastsubstring = line[lastpos:newlinepos]
+    alleventslines.append(lastsubstring)
+    count += 1
+in_file.close()
+print("Count eventslines", len(alleventslines))
+for i in range(len(alleventslines)):
+    neweventpos = alleventslines[i].find("BEGIN:VEVENT")
+    summaryeventpos = alleventslines[i].find("SUMMARY")
+    descriptioneventpos = alleventslines[i].find("DESCRIPTION")
+    locationeventpos = alleventslines[i].find("LOCATION")
+    dtstarteventpos = alleventslines[i].find("DTSTART")
+    dtendeventpos = alleventslines[i].find("DTEND")
+    endeventpos = alleventslines[i].find("END:VEVENT")
+    if neweventpos == 0:
+        day = 0
+        description = ""
+        location = ""
+        starttime = 0
+        endtime = 0
+        month = 0
+    if dtstarteventpos == 0:
+        eventdtstartstr = alleventslines[i][8:]
+        datevaluepos = alleventslines[i].find("VALUE=DATE:")
+        if datevaluepos == 8:
+            eventdtstartstr = alleventslines[i][19:]
+        year = int(eventdtstartstr[:4])
+        month = int(eventdtstartstr[4:6])
+        day = int(eventdtstartstr[6:8])
+        starttime = eventdtstartstr
+    if dtendeventpos == 0:
+        eventdtendstr = alleventslines[i][6:]
+        endtime = eventdtendstr[9:11] + ':' + eventdtendstr[11:13]
+    if summaryeventpos == 0:
+        summary = alleventslines[i][8:]
+    if descriptioneventpos == 0:
+        description = alleventslines[i][12:]
+    if locationeventpos == 0:
+        location = alleventslines[i][9:]
+    if endeventpos == 0:
+        gameevents.append(GameEvent(summary, day, description, location, starttime, endtime, month))
+print("Count game events", len(gameevents))
+
+key = input("Wait")
