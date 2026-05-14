1f139e32a838b9dc95657c1cb4dec9e306dad8ab
jan438 <jan438@users.noreply.github.com>
2026-05-14 11:04:30 +0200

    print added

--- processics.py
+++ processics.py
@@ -103,6 +103,7 @@ for i in range(len(gameevents)):
         second=0,
         tzinfo=timezone(timedelta(seconds=7200))
     )
+    print(gameevents[i].endtime)
     e.end = datetime(
         year=2026,
         month=gameevents[i].month,
