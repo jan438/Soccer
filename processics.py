5c2ef254e533d09ec4a023b62e00c27986de4048
jan438 <jan438@users.noreply.github.com>
2026-05-14 09:53:34 +0200

    begin renamed

--- processics.py
+++ processics.py
@@ -93,8 +93,8 @@ for i in range(len(gameevents)):
     e.description = gameevents[i].description
     e.location = gameevents[i].description
     e.begin = datetime.fromisoformat("2022-06-06T12:05:23+02:00")
-    [hour, minute] = converttimetztolocalclock(gameevents[i].starttime)
-    print(gameevents[i].starttime, hour, minute)
+    [hourb, minuteb] = converttimetztolocalclock(gameevents[i].starttime)
+    print(gameevents[i].starttime, hourb, minuteb)
     e.end = datetime(
         year=2026,
         month=gameevents[i].month,
