371d8b39a8f464023310074015626684075e60ff
jan438 <jan438@users.noreply.github.com>
2026-05-16 10:57:57 +0200

    event added

--- icstz.py
+++ icstz.py
@@ -3,5 +3,11 @@ from datetime import datetime as dt
 from ics import Event, Calendar
 import os, time
 import pytz
-    
+
+t = dt(2020, 4, 1, 18, 0)
+e = Event()
+e.begin
+
+print(e)
+
 key = input("Wait")
