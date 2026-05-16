be9b8bdbc0307a92bc882c4fd15114aba49d2df1
jan438 <jan438@users.noreply.github.com>
2026-05-16 14:02:53 +0200

    test added

--- icstz.py
+++ icstz.py
@@ -7,10 +7,17 @@ import pytz
 c = Calendar()
 t = dt(2020, 4, 1, 18, 0)
 e = Event(begin=t)
+
 c.events.add(e)
 
 with open("Calendar/WK2026_test_lines.ics", "w") as f:
     f.writelines(c)
     f.close()
     
+c = Calendar()
+
+with open("Calendar/WK2026_test_lines.ics", "r") as f:
+    c = f.readlines()
+    f.close()
+
 key = input("Wait")
