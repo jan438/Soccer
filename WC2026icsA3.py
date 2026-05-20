894a9431745d31d11290784dc9a657a151064c37
jan438 <jan438@users.noreply.github.com>
2026-05-20 09:41:24 +0200

    title greater

--- WC2026icsA3.py
+++ WC2026icsA3.py
@@ -255,12 +255,12 @@ drawing = scaleSVG("SVG/WorldMap" + mapversie + ".svg", scalesimple)
 renderPDF.draw(drawing, my_canvas, xsimple, ysimple)
     
 drawing = scaleSVG('FIFA.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 50, 800)
+renderPDF.draw(drawing, my_canvas, 75, 1000)
 
-my_canvas.setFont(socfont, 25)
+my_canvas.setFont(socfont, 40)
 my_canvas.setFillColor(HexColor("#000000"))
 my_canvas.setTitle("World Cup Soccer 2026 " + version)
-my_canvas.drawString(200, 805, "World Cup Soccer 2026")
+my_canvas.drawString(200, 1005, "World Cup Soccer 2026")
 
 for poule in range(12):
     if poule % 2 == 0:
