4a7417c9652c7400fce7c7b1f245c71063b1a49d
jan438 <jan438@users.noreply.github.com>
2026-05-20 10:29:09 +0200

    good pos

--- WC2026icsA3.py
+++ WC2026icsA3.py
@@ -297,7 +297,7 @@ for poule in range(12):
     pouleland_y = 875
     my_canvas.setFont(socfont, 35)
     my_canvas.setFillColor(HexColor("#000000"))
-    my_canvas.drawString(left_margin + poule * poule_width + 17.0, poulerect_y + poule_height - 25, chr(65 + poule))
+    my_canvas.drawString(left_margin + poule * poule_width + 17.0, poulerect_y + poule_height - 30, chr(65 + poule))
     for team in range(teamspp):
         nameinlogo = nationsdata[teamcounter][4]
         if cadre_pouleland:
