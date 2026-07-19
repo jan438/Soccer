cb50bc1fa1962c8cbcd45b9b7700dcd77bb16131
jan438 <jan438@users.noreply.github.com>
2026-07-19 14:12:31 +0200

    Update WC2026new.py

--- WC2026new.py
+++ WC2026new.py
@@ -457,8 +457,10 @@ for i in range(48):
             ccy = ccy + 8
         my_canvas.circle(297 + (i % 8) * 8, ccy, 3.0, stroke = 0, fill = 1)
     teamcounter += 1
-renderPDF.draw(scaleSVG("SVG/Bronze_medal.svg", 0.06), my_canvas, 530, 140)
-renderPDF.draw(scaleSVG("SVG/Silver_medal.svg", 0.06), my_canvas, 530, 160)
-renderPDF.draw(scaleSVG("SVG/Gold_medal.svg", 0.06), my_canvas, 530, 180)
+renderPDF.draw(scaleSVG("SVG/Bronze_medal.svg", 0.06), my_canvas, 520, 140)
+renderPDF.draw(scaleSVG("SVG/Silver_medal.svg", 0.06), my_canvas, 520, 160)
+renderPDF.draw(scaleSVG("SVG/Gold_medal.svg", 0.06), my_canvas, 520, 180)
+my_canvas.setFont(socfont, 13)
+my_canvas.drawString(538,140,"Engeland")
 my_canvas.save()
 key = input("Wait")
